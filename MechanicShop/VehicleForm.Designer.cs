b3f8f34987c03f33322f90bbc62d8a355deeb810
Mauzy0x <mauzy.mail@proton.me>
2024-03-26 16:55:15 -0400

    Implement customer search in Vechicle Form

--- MechanicShop/VehicleForm.Designer.cs
+++ MechanicShop/VehicleForm.Designer.cs
@@ -44,155 +44,170 @@
             txt_vehicleTrim = new TextBox();
             txt_vehicleModel = new TextBox();
             txt_vehicleMake = new TextBox();
-            cbBox_vehicleCustomer = new ComboBox();
             btn_vehicleAdd = new Button();
             btn_vehicleCancel = new Button();
+            txt_customer_firstName = new TextBox();
+            txt_customer_lastName = new TextBox();
+            btn_customer_search = new Button();
+            dataGrid_customer_search_results = new DataGridView();
+            lbl_customerID = new Label();
+            txt_customerID = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)dataGrid_customer_search_results).BeginInit();
             SuspendLayout();
             // 
             // lbl_AddCustomerTitle
             // 
             lbl_AddCustomerTitle.AutoSize = true;
             lbl_AddCustomerTitle.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            lbl_AddCustomerTitle.Location = new Point(9, 8);
+            lbl_AddCustomerTitle.Location = new Point(11, 10);
+            lbl_AddCustomerTitle.Margin = new Padding(4, 0, 4, 0);
             lbl_AddCustomerTitle.Name = "lbl_AddCustomerTitle";
-            lbl_AddCustomerTitle.Size = new Size(181, 41);
+            lbl_AddCustomerTitle.Size = new Size(216, 48);
             lbl_AddCustomerTitle.TabIndex = 40;
             lbl_AddCustomerTitle.Text = "Vehicle Data";
             // 
             // lbl_vehicleMake
             // 
             lbl_vehicleMake.AutoSize = true;
-            lbl_vehicleMake.Location = new Point(12, 109);
+            lbl_vehicleMake.Location = new Point(15, 230);
+            lbl_vehicleMake.Margin = new Padding(4, 0, 4, 0);
             lbl_vehicleMake.Name = "lbl_vehicleMake";
-            lbl_vehicleMake.Size = new Size(45, 20);
+            lbl_vehicleMake.Size = new Size(55, 25);
             lbl_vehicleMake.TabIndex = 38;
             lbl_vehicleMake.Text = "Make";
             // 
             // lbl_vehicleVIN
             // 
             lbl_vehicleVIN.AutoSize = true;
-            lbl_vehicleVIN.Location = new Point(12, 307);
+            lbl_vehicleVIN.Location = new Point(15, 478);
+            lbl_vehicleVIN.Margin = new Padding(4, 0, 4, 0);
             lbl_vehicleVIN.Name = "lbl_vehicleVIN";
-            lbl_vehicleVIN.Size = new Size(33, 20);
+            lbl_vehicleVIN.Size = new Size(41, 25);
             lbl_vehicleVIN.TabIndex = 37;
             lbl_vehicleVIN.Text = "VIN";
             // 
             // lbl_vehicleColor
             // 
             lbl_vehicleColor.AutoSize = true;
-            lbl_vehicleColor.Location = new Point(12, 274);
+            lbl_vehicleColor.Location = new Point(15, 436);
+            lbl_vehicleColor.Margin = new Padding(4, 0, 4, 0);
             lbl_vehicleColor.Name = "lbl_vehicleColor";
-            lbl_vehicleColor.Size = new Size(45, 20);
+            lbl_vehicleColor.Size = new Size(55, 25);
             lbl_vehicleColor.TabIndex = 36;
             lbl_vehicleColor.Text = "Color";
             // 
             // lbl_vehicleLicensePlate
             // 
             lbl_vehicleLicensePlate.AutoSize = true;
-            lbl_vehicleLicensePlate.Location = new Point(12, 241);
+            lbl_vehicleLicensePlate.Location = new Point(15, 395);
+            lbl_vehicleLicensePlate.Margin = new Padding(4, 0, 4, 0);
             lbl_vehicleLicensePlate.Name = "lbl_vehicleLicensePlate";
-            lbl_vehicleLicensePlate.Size = new Size(94, 20);
+            lbl_vehicleLicensePlate.Size = new Size(111, 25);
             lbl_vehicleLicensePlate.TabIndex = 35;
             lbl_vehicleLicensePlate.Text = "License Plate";
             // 
             // lbl_vehicleYear
             // 
             lbl_vehicleYear.AutoSize = true;
-            lbl_vehicleYear.Location = new Point(12, 208);
+            lbl_vehicleYear.Location = new Point(15, 354);
+            lbl_vehicleYear.Margin = new Padding(4, 0, 4, 0);
             lbl_vehicleYear.Name = "lbl_vehicleYear";
-            lbl_vehicleYear.Size = new Size(37, 20);
+            lbl_vehicleYear.Size = new Size(44, 25);
             lbl_vehicleYear.TabIndex = 34;
             lbl_vehicleYear.Text = "Year";
             // 
             // lbl_vehicleTrim
             // 
             lbl_vehicleTrim.AutoSize = true;
-            lbl_vehicleTrim.Location = new Point(12, 175);
+            lbl_vehicleTrim.Location = new Point(15, 313);
+            lbl_vehicleTrim.Margin = new Padding(4, 0, 4, 0);
             lbl_vehicleTrim.Name = "lbl_vehicleTrim";
-            lbl_vehicleTrim.Size = new Size(38, 20);
+            lbl_vehicleTrim.Size = new Size(45, 25);
             lbl_vehicleTrim.TabIndex = 33;
             lbl_vehicleTrim.Text = "Trim";
             // 
             // lbl_vehicleModel
             // 
             lbl_vehicleModel.AutoSize = true;
-            lbl_vehicleModel.Location = new Point(12, 142);
+            lbl_vehicleModel.Location = new Point(15, 272);
+            lbl_vehicleModel.Margin = new Padding(4, 0, 4, 0);
             lbl_vehicleModel.Name = "lbl_vehicleModel";
-            lbl_vehicleModel.Size = new Size(52, 20);
+            lbl_vehicleModel.Size = new Size(63, 25);
             lbl_vehicleModel.TabIndex = 32;
             lbl_vehicleModel.Text = "Model";
             // 
             // lbl_customer
             // 
             lbl_customer.AutoSize = true;
-            lbl_customer.Location = new Point(9, 71);
+            lbl_customer.Location = new Point(11, 89);
+            lbl_customer.Margin = new Padding(4, 0, 4, 0);
             lbl_customer.Name = "lbl_customer";
-            lbl_customer.Size = new Size(72, 20);
+            lbl_customer.Size = new Size(140, 25);
             lbl_customer.TabIndex = 31;
-            lbl_customer.Text = "Customer";
+            lbl_customer.Text = "Select Customer";
             // 
             // txt_vehicleVIN
             // 
-            txt_vehicleVIN.Location = new Point(119, 304);
+            txt_vehicleVIN.Location = new Point(149, 474);
+            txt_vehicleVIN.Margin = new Padding(4);
             txt_vehicleVIN.Name = "txt_vehicleVIN";
-            txt_vehicleVIN.Size = new Size(199, 27);
+            txt_vehicleVIN.Size = new Size(248, 31);
             txt_vehicleVIN.TabIndex = 29;
             // 
             // txt_vehicleColor
             // 
-            txt_vehicleColor.Location = new Point(119, 271);
+            txt_vehicleColor.Location = new Point(149, 433);
+            txt_vehicleColor.Margin = new Padding(4);
             txt_vehicleColor.Name = "txt_vehicleColor";
-            txt_vehicleColor.Size = new Size(199, 27);
+            txt_vehicleColor.Size = new Size(248, 31);
             txt_vehicleColor.TabIndex = 28;
             // 
             // txt_vehicleLicensePlate
             // 
-            txt_vehicleLicensePlate.Location = new Point(119, 238);
+            txt_vehicleLicensePlate.Location = new Point(149, 392);
+            txt_vehicleLicensePlate.Margin = new Padding(4);
             txt_vehicleLicensePlate.Name = "txt_vehicleLicensePlate";
-            txt_vehicleLicensePlate.Size = new Size(199, 27);
+            txt_vehicleLicensePlate.Size = new Size(248, 31);
             txt_vehicleLicensePlate.TabIndex = 27;
             // 
             // txt_vehicleYear
             // 
-            txt_vehicleYear.Location = new Point(119, 205);
+            txt_vehicleYear.Location = new Point(149, 350);
+            txt_vehicleYear.Margin = new Padding(4);
             txt_vehicleYear.Name = "txt_vehicleYear";
-            txt_vehicleYear.Size = new Size(199, 27);
+            txt_vehicleYear.Size = new Size(248, 31);
             txt_vehicleYear.TabIndex = 26;
             // 
             // txt_vehicleTrim
             // 
-            txt_vehicleTrim.Location = new Point(119, 172);
+            txt_vehicleTrim.Location = new Point(149, 309);
+            txt_vehicleTrim.Margin = new Padding(4);
             txt_vehicleTrim.Name = "txt_vehicleTrim";
-            txt_vehicleTrim.Size = new Size(199, 27);
+            txt_vehicleTrim.Size = new Size(248, 31);
             txt_vehicleTrim.TabIndex = 25;
             // 
             // txt_vehicleModel
             // 
-            txt_vehicleModel.Location = new Point(119, 139);
+            txt_vehicleModel.Location = new Point(149, 268);
+            txt_vehicleModel.Margin = new Padding(4);
             txt_vehicleModel.Name = "txt_vehicleModel";
-            txt_vehicleModel.Size = new Size(199, 27);
+            txt_vehicleModel.Size = new Size(248, 31);
             txt_vehicleModel.TabIndex = 24;
             // 
             // txt_vehicleMake
             // 
-            txt_vehicleMake.Location = new Point(119, 106);
+            txt_vehicleMake.Location = new Point(149, 226);
+            txt_vehicleMake.Margin = new Padding(4);
             txt_vehicleMake.Name = "txt_vehicleMake";
-            txt_vehicleMake.Size = new Size(199, 27);
+            txt_vehicleMake.Size = new Size(248, 31);
             txt_vehicleMake.TabIndex = 23;
-            // 
-            // cbBox_vehicleCustomer
-            // 
-            cbBox_vehicleCustomer.FormattingEnabled = true;
-            cbBox_vehicleCustomer.Location = new Point(119, 68);
-            cbBox_vehicleCustomer.Name = "cbBox_vehicleCustomer";
-            cbBox_vehicleCustomer.Size = new Size(199, 28);
-            cbBox_vehicleCustomer.TabIndex = 41;
             // 
             // btn_vehicleAdd
             // 
-            btn_vehicleAdd.Location = new Point(12, 432);
+            btn_vehicleAdd.Location = new Point(15, 540);
+            btn_vehicleAdd.Margin = new Padding(4);
             btn_vehicleAdd.Name = "btn_vehicleAdd";
-            btn_vehicleAdd.Size = new Size(94, 29);
+            btn_vehicleAdd.Size = new Size(118, 36);
             btn_vehicleAdd.TabIndex = 42;
             btn_vehicleAdd.Text = "Add Vehicle";
             btn_vehicleAdd.UseVisualStyleBackColor = true;
@@ -200,22 +215,79 @@
             // 
             // btn_vehicleCancel
             // 
-            btn_vehicleCancel.Location = new Point(121, 432);
+            btn_vehicleCancel.Location = new Point(151, 540);
+            btn_vehicleCancel.Margin = new Padding(4);
             btn_vehicleCancel.Name = "btn_vehicleCancel";
-            btn_vehicleCancel.Size = new Size(94, 29);
+            btn_vehicleCancel.Size = new Size(118, 36);
             btn_vehicleCancel.TabIndex = 43;
             btn_vehicleCancel.Text = "Cancel";
             btn_vehicleCancel.UseVisualStyleBackColor = true;
             btn_vehicleCancel.Click += btn_vehicleCancel_Click;
             // 
+            // txt_customer_firstName
+            // 
+            txt_customer_firstName.Location = new Point(15, 117);
+            txt_customer_firstName.Name = "txt_customer_firstName";
+            txt_customer_firstName.Size = new Size(178, 31);
+            txt_customer_firstName.TabIndex = 44;
+            txt_customer_firstName.Text = "First Name";
+            // 
+            // txt_customer_lastName
+            // 
+            txt_customer_lastName.Location = new Point(199, 117);
+            txt_customer_lastName.Name = "txt_customer_lastName";
+            txt_customer_lastName.Size = new Size(198, 31);
+            txt_customer_lastName.TabIndex = 45;
+            txt_customer_lastName.Text = "Last Name";
+            // 
+            // btn_customer_search
+            // 
+            btn_customer_search.Location = new Point(403, 115);
+            btn_customer_search.Name = "btn_customer_search";
+            btn_customer_search.Size = new Size(112, 34);
+            btn_customer_search.TabIndex = 46;
+            btn_customer_search.Text = "Search";
+            btn_customer_search.UseVisualStyleBackColor = true;
+            btn_customer_search.Click += btn_customer_search_Click;
+            // 
+            // dataGrid_customer_search_results
+            // 
+            dataGrid_customer_search_results.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGrid_customer_search_results.Location = new Point(532, 115);
+            dataGrid_customer_search_results.Name = "dataGrid_customer_search_results";
+            dataGrid_customer_search_results.RowHeadersWidth = 62;
+            dataGrid_customer_search_results.Size = new Size(739, 388);
+            dataGrid_customer_search_results.TabIndex = 47;
+            // 
+            // lbl_customerID
+            // 
+            lbl_customerID.AutoSize = true;
+            lbl_customerID.Location = new Point(15, 186);
+            lbl_customerID.Name = "lbl_customerID";
+            lbl_customerID.Size = new Size(112, 25);
+            lbl_customerID.TabIndex = 48;
+            lbl_customerID.Text = "Customer ID";
+            // 
+            // txt_customerID
+            // 
+            txt_customerID.Location = new Point(149, 186);
+            txt_customerID.Name = "txt_customerID";
+            txt_customerID.Size = new Size(150, 31);
+            txt_customerID.TabIndex = 49;
+            // 
             // VehicleForm
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(611, 473);
+            ClientSize = new Size(1294, 588);
+            Controls.Add(txt_customerID);
+            Controls.Add(lbl_customerID);
+            Controls.Add(dataGrid_customer_search_results);
+            Controls.Add(btn_customer_search);
+            Controls.Add(txt_customer_lastName);
+            Controls.Add(txt_customer_firstName);
             Controls.Add(btn_vehicleCancel);
             Controls.Add(btn_vehicleAdd);
-            Controls.Add(cbBox_vehicleCustomer);
             Controls.Add(lbl_AddCustomerTitle);
             Controls.Add(lbl_vehicleMake);
             Controls.Add(lbl_vehicleVIN);
@@ -232,9 +304,11 @@
             Controls.Add(txt_vehicleTrim);
             Controls.Add(txt_vehicleModel);
             Controls.Add(txt_vehicleMake);
+            Margin = new Padding(4);
             Name = "VehicleForm";
             Text = "Vehicles";
             Load += VehicleForm_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGrid_customer_search_results).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -257,8 +331,13 @@
         private TextBox txt_vehicleTrim;
         private TextBox txt_vehicleModel;
         private TextBox txt_vehicleMake;
-        private ComboBox cbBox_vehicleCustomer;
         private Button btn_vehicleAdd;
         private Button btn_vehicleCancel;
+        private TextBox txt_customer_firstName;
+        private TextBox txt_customer_lastName;
+        private Button btn_customer_search;
+        private DataGridView dataGrid_customer_search_results;
+        private Label lbl_customerID;
+        private TextBox txt_customerID;
     }
 }
--- MechanicShop/VehicleForm.cs
+++ MechanicShop/VehicleForm.cs
@@ -10,6 +10,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using static MechanicShop.NewCustomerForm;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace MechanicShop
 {
@@ -19,7 +20,7 @@ namespace MechanicShop
 
         public class Vehicle
         {
-            public string Customer { get; set; } = string.Empty;
+            public int CustomerID { get; set; } = 0;
             public string Make { get; set; } = string.Empty;
             public string Model { get; set; } = string.Empty;
             public string Trim { get; set; } = string.Empty;
@@ -37,83 +38,64 @@ namespace MechanicShop
 
         private void VehicleForm_Load(object sender, EventArgs e)
         {
-            // Retrieve customer names from the database and populate the ComboBox
-            PopulateCustomerComboBox();
+
         }
-
-        // Populate a ComboBox with the first and last names from the mechanicshop database
-        // format:{lastName}, {firstName}
-        private void PopulateCustomerComboBox()
-        { 
-            string query = "SELECT customer_Fname, customer_Lname FROM Customers";
-            SqlCommand command = new SqlCommand(query, connection);
-
-            // Read the results and add each customer name to the ComboBox
-            using (SqlDataReader reader = command.ExecuteReader())
-            {
-                while (reader.Read())
-                {
-                    // Get the string (assmued data type is string. Must be in the string format. Use a different Get method for different data types)
-                    // The number passed to the method is the index of the column retrieved in the SQL query
-                    string firstName = reader.GetString(0);
-                    string lastName = reader.GetString(1);
-
-                    string fullName = $"{lastName}, {firstName}";
-                    // Add each customer name to the ComboBox
-                    cbBox_vehicleCustomer.Items.Add(fullName);
-                    // Reference: https://learn.microsoft.com/en-us/dotnet/api/system.data.sqlclient.sqldatareader.getsqlstring?view=dotnet-plat-ext-8.0
-                }
-                reader.Close(); // Only one SqlDataReader per associated SqlConnection may be open at a time. Be sure to call Close()
-            }
-        } // Reference: https://learn.microsoft.com/en-us/dotnet/api/system.data.sqlclient.sqldatareader.read?view=dotnet-plat-ext-8.0
 
         private void btn_vehicleAdd_Click(object sender, EventArgs e)
         {
-            Vehicle vehicle = new Vehicle()
+            // Make sure the user entered an integer for the customerID
+            if (int.TryParse(txt_customerID.Text, out int int_customerID) && int_customerID != 0)
             {
-                Customer = cbBox_vehicleCustomer.Text,
-                Make = txt_vehicleMake.Text,
-                Model = txt_vehicleModel.Text,
-                Trim = txt_vehicleTrim.Text,
-                Year = txt_vehicleYear.Text,
-                LicensePlate = txt_vehicleLicensePlate.Text,
-                Color = txt_vehicleColor.Text,
-                VIN = txt_vehicleVIN.Text,
-            };
+                Vehicle vehicle = new Vehicle()
+                {
+                    CustomerID = int_customerID,
+                    Make = txt_vehicleMake.Text,
+                    Model = txt_vehicleModel.Text,
+                    Trim = txt_vehicleTrim.Text,
+                    Year = txt_vehicleYear.Text,
+                    LicensePlate = txt_vehicleLicensePlate.Text,
+                    Color = txt_vehicleColor.Text,
+                    VIN = txt_vehicleVIN.Text,
+                };
 
-            // Have user confirm before saving
-            DialogResult result = MessageBox.Show("Insert Vehicle into Database?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                // Have user confirm before saving
+                DialogResult result = MessageBox.Show("Insert Vehicle into Database?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
 
-            if (result == DialogResult.Yes)
+                if (result == DialogResult.Yes)
+                {
+                    string p_insert_vehicle = @"dbo.[insert_vehicles]";
+                    SqlCommand command = new SqlCommand(p_insert_vehicle, connection);
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    // Set SQL Parameters
+                    command.Parameters.Clear();
+                    SqlParameter p_Customer = new SqlParameter("@p_customer_id", vehicle.CustomerID);
+                    SqlParameter p_Make = new SqlParameter("@p_vehicle_make", vehicle.Make);
+                    SqlParameter p_Model = new SqlParameter("@vehicle_model", vehicle.Model);
+                    SqlParameter p_Trim = new SqlParameter("@p_vehicle_trim", vehicle.Trim);
+                    SqlParameter p_Year = new SqlParameter("@p_vehicle_year", vehicle.Year);
+                    SqlParameter p_LicensePlate = new SqlParameter("@p_vehicle_license_plate", vehicle.LicensePlate);
+                    SqlParameter p_Color = new SqlParameter("@p_vehicle_color", vehicle.Color);
+                    SqlParameter p_VIN = new SqlParameter("@p_vehicle_vin", vehicle.VIN);
+
+                    // Add the parameters to the SqlCommand Object
+                    command.Parameters.Add(p_Customer);
+                    command.Parameters.Add(p_Make);
+                    command.Parameters.Add(p_Model);
+                    command.Parameters.Add(p_Trim);
+                    command.Parameters.Add(p_Year);
+                    command.Parameters.Add(p_LicensePlate);
+                    command.Parameters.Add(p_Color);
+                    command.Parameters.Add(p_VIN);
+
+                    // EXEC the procedure
+                    command.ExecuteNonQuery();
+                    this.Close();
+                }
+            }
+            else
             {
-                string p_insert_vehicle = @"dbo.[insert_vehicles]";
-                SqlCommand command = new SqlCommand(p_insert_vehicle, connection);
-                command.CommandType = CommandType.StoredProcedure;
-
-                // Set SQL Parameters
-                command.Parameters.Clear();
-                SqlParameter p_Customer = new SqlParameter("@p_customer", vehicle.Customer);
-                SqlParameter p_Make = new SqlParameter("@p_make", vehicle.Make);
-                SqlParameter p_Model = new SqlParameter("@p_model", vehicle.Model);
-                SqlParameter p_Trim = new SqlParameter("@p_trim", vehicle.Trim);
-                SqlParameter p_Year = new SqlParameter("@p_year", vehicle.Year);
-                SqlParameter p_LicensePlate = new SqlParameter("@p_license_plate", vehicle.LicensePlate);
-                SqlParameter p_Color = new SqlParameter("@p_color", vehicle.Color);
-                SqlParameter p_VIN = new SqlParameter("@p_vin", vehicle.VIN);
-
-                // Add the parameters to the SqlCommand Object
-                command.Parameters.Add(p_Customer);
-                command.Parameters.Add(p_Make);
-                command.Parameters.Add(p_Model);
-                command.Parameters.Add(p_Trim);
-                command.Parameters.Add(p_Year);
-                command.Parameters.Add(p_LicensePlate);
-                command.Parameters.Add(p_Color);
-                command.Parameters.Add(p_VIN);
-
-                // EXEC the procedure
-                command.ExecuteNonQuery();
-                this.Close();
+                MessageBox.Show("Customer ID must be a non 0 number!", "Bad Value", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
             }
         }
 
@@ -125,6 +107,45 @@ namespace MechanicShop
             if (result == DialogResult.Yes)
             {
                 this.Close();
+            }
+        }
+
+        // Use the given First and Last names to search for customers in the DataBase
+        private void btn_customer_search_Click(object sender, EventArgs e)
+        {
+            string FirstName = txt_customer_firstName.Text;
+            string LastName = txt_customer_lastName.Text;
+
+            string functionName = @"dbo.[search_customer_byName]";
+
+            using (SqlCommand command = new SqlCommand($"SELECT * FROM {functionName}(@p_customer_Fname, @p_customer_Lname)", connection))
+            {
+
+                // Set SQL Parameters
+                command.Parameters.Clear();
+                SqlParameter p_FirstName = new SqlParameter("@p_customer_Fname", FirstName);
+                SqlParameter p_LastName = new SqlParameter("@p_customer_Lname", LastName);
+
+                // Add the parameters to the SqlCommand Object
+                command.Parameters.Add(p_FirstName);
+                command.Parameters.Add(p_LastName);
+
+
+
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                DataTable dataTable = new DataTable();
+
+                try
+                {
+                    // Fill the data grid
+                    adapter.Fill(dataTable);
+                    dataGrid_customer_search_results.DataSource = dataTable;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+
             }
         }
     }
